5786c0a191a18973efccd02bc734ed1c62e13e39
tokuhirom <tokuhirom@gmail.com>
2010-10-08 17:23:34 +0900

    initial import

---
+++ lib/Cache/KyotoTycoon/REST.pm
@@ -0,0 +1,39 @@
+package Cache::KyotoTycoon::REST;
+use strict;
+use warnings;
+use 5.00800;
+our $VERSION = '0.01';
+
+
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+Cache::KyotoTycoon::REST -
+
+=head1 SYNOPSIS
+
+  use Cache::KyotoTycoon::REST;
+
+=head1 DESCRIPTION
+
+Cache::KyotoTycoon::REST is
+
+=head1 AUTHOR
+
+Tokuhiro Matsuno E<lt>tokuhirom AAJKLFJEF GMAIL COME<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+Copyright (C) Tokuhiro Matsuno
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
